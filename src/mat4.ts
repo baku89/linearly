14a3f529dbb6d1a1293c1ce4c2754e0ea0021ba3
Baku Hashimoto <mail@baku89.com>
2023-09-12 22:31:30 +0900

    Fix linting errors

--- src/mat4.ts
+++ src/mat4.ts
@@ -290,51 +290,36 @@ export function scale(a: Mat4, v: Vec3): Mat4 {
  */
 export function rotate(a: Mat4, rad: number, axis: Vec3): Mat4 | null {
 	let [x, y, z] = axis
-	let len = Math.sqrt(x * x + y * y + z * z)
-	let s, c, t
-	let a00, a01, a02, a03
-	let a10, a11, a12, a13
-	let a20, a21, a22, a23
-	let b00, b01, b02
-	let b10, b11, b12
-	let b20, b21, b22
+	const len = Math.sqrt(x * x + y * y + z * z)
 
 	if (len < Common.EPSILON) {
 		return null
 	}
 
-	len = 1 / len
-	x *= len
-	y *= len
-	z *= len
+	x /= len
+	y /= len
+	z /= len
 
-	s = Math.sin(rad)
-	c = Math.cos(rad)
-	t = 1 - c
+	const s = Math.sin(rad)
+	const c = Math.cos(rad)
+	const t = 1 - c
 
-	a00 = a[0]
-	a01 = a[1]
-	a02 = a[2]
-	a03 = a[3]
-	a10 = a[4]
-	a11 = a[5]
-	a12 = a[6]
-	a13 = a[7]
-	a20 = a[8]
-	a21 = a[9]
-	a22 = a[10]
-	a23 = a[11]
+	// prettier-ignore
+	const [
+		a00, a01, a02, a03,
+		a10, a11, a12, a13,
+		a20, a21, a22, a23] = a
 
 	// Construct the elements of the rotation matrix
-	b00 = x * x * t + c
-	b01 = y * x * t + z * s
-	b02 = z * x * t - y * s
-	b10 = x * y * t - z * s
-	b11 = y * y * t + c
-	b12 = z * y * t + x * s
-	b20 = x * z * t + y * s
-	b21 = y * z * t - x * s
-	b22 = z * z * t + c
+	const b00 = x * x * t + c
+	const b01 = y * x * t + z * s
+	const b02 = z * x * t - y * s
+	const b10 = x * y * t - z * s
+	const b11 = y * y * t + c
+	const b12 = z * y * t + x * s
+	const b20 = x * z * t + y * s
+	const b21 = y * z * t - x * s
+	const b22 = z * z * t + c
 
 	// prettier-ignore
 	return [
@@ -473,21 +458,19 @@ export function fromScaling(v: Vec3): Mat4 {
  */
 export function fromRotation(rad: number, axis: Vec3): Mat4 | null {
 	let [x, y, z] = axis
-	let len = Math.sqrt(x * x + y * y + z * z)
-	let s, c, t
+	const len = Math.sqrt(x * x + y * y + z * z)
 
 	if (len < Common.EPSILON) {
 		return null
 	}
 
-	len = 1 / len
-	x *= len
-	y *= len
-	z *= len
+	x /= len
+	y /= len
+	z /= len
 
-	s = Math.sin(rad)
-	c = Math.cos(rad)
-	t = 1 - c
+	const s = Math.sin(rad)
+	const c = Math.cos(rad)
+	const t = 1 - c
 
 	// prettier-ignore
 	return [
@@ -956,7 +939,7 @@ export function perspectiveNO(
 
 	let out10: number, out14: number
 
-	if (far != null && far !== Infinity) {
+	if (far !== null && far !== Infinity) {
 		const nf = 1 / (near - far)
 		out10 = (far + near) * nf
 		out14 = 2 * far * near * nf
@@ -995,13 +978,13 @@ export function perspectiveZO(
 	fovy: number,
 	aspect: number,
 	near: number,
-	far: number
+	far: number | null
 ): Mat4 {
 	const f = 1.0 / Math.tan(fovy / 2)
 
 	let out10: number, out14: number
 
-	if (far != null && far !== Infinity) {
+	if (far !== null && far !== Infinity) {
 		const nf = 1 / (near - far)
 		out10 = far * nf
 		out14 = far * near * nf
