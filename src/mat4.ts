4383d7e83aacd7a3f72c735a5d5b43f20a916f32
Baku Hashimoto <mail@baku89.com>
2023-09-12 22:21:22 +0900

    Refactor mat4.multiply

--- src/mat4.ts
+++ src/mat4.ts
@@ -196,24 +196,14 @@ export function determinant(a: Mat4) {
  * Multiplies two mat4's
  */
 export function multiply(a: Mat4, b: Mat4): Mat4 {
-	const a00 = a[0],
-		a01 = a[1],
-		a02 = a[2],
-		a03 = a[3]
-	const a10 = a[4],
-		a11 = a[5],
-		a12 = a[6],
-		a13 = a[7]
-	const a20 = a[8],
-		a21 = a[9],
-		a22 = a[10],
-		a23 = a[11]
-	const a30 = a[12],
-		a31 = a[13],
-		a32 = a[14],
-		a33 = a[15]
+	// prettier-ignore
+	const [
+		a00, a01, a02, a03,
+		a10, a11, a12, a13,
+		a20, a21, a22, a23,
+		a30, a31, a32, a33] = a
 
-	const out = Array(16).fill(0)
+	const out = Array(16)
 
 	// Cache only the current line of the second matrix
 	let [b0, b1, b2, b3] = b
@@ -250,7 +240,7 @@ export function multiply(a: Mat4, b: Mat4): Mat4 {
 	out[14] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
 	out[15] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
 
-	return out as any as Mat4
+	return out as any
 }
 
 /**
