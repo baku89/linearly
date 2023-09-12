0f5bdb32e253f5936e20e6a5a95cd75ab9dd944a
Baku Hashimoto <mail@baku89.com>
2023-09-13 01:26:05 +0900

    Refactor mat2d.test.ts

--- src/mat2d.test.ts
+++ src/mat2d.test.ts
@@ -21,93 +21,92 @@ function areMat2dEqual(a: unknown, b: unknown) {
 
 ;(expect as any).addEqualityTesters([areMat2dEqual])
 
-describe('mat2d', () => {
-	const matA: Mat2d = [1, 2, 3, 4, 5, 6]
-	const matB: Mat2d = [7, 8, 9, 10, 11, 12]
+const matA: Mat2d = [1, 2, 3, 4, 5, 6]
+const matB: Mat2d = [7, 8, 9, 10, 11, 12]
 
-	describe('invert', () => {
-		expect(mat2d.invert(matA)).toEqual([-2, 1, 1.5, -0.5, 1, -2])
-	})
+describe('invert', () => {
+	expect(mat2d.invert(matA)).toEqual([-2, 1, 1.5, -0.5, 1, -2])
+	expect(mat2d.invert([1, 0, 2, 0, 0, 0])).toEqual(0)
+})
 
-	describe('determinant', () => {
-		expect(mat2d.determinant(matA)).toEqual(-2)
-	})
+describe('determinant', () => {
+	expect(mat2d.determinant(matA)).toEqual(-2)
+})
 
-	describe('multiply', () => {
-		expect(mat2d.multiply(matA, matB)).toEqual([31, 46, 39, 58, 52, 76])
-	})
+describe('multiply', () => {
+	expect(mat2d.multiply(matA, matB)).toEqual([31, 46, 39, 58, 52, 76])
+})
 
-	describe('rotate', () => {
-		expect(mat2d.rotate(matA, Math.PI * 0.5)).toEqual([3, 4, -1, -2, 5, 6])
-	})
+describe('rotate', () => {
+	expect(mat2d.rotate(matA, Math.PI * 0.5)).toEqual([3, 4, -1, -2, 5, 6])
+})
 
-	describe('scale', () => {
-		expect(mat2d.scale(matA, [2, 3])).toEqual([2, 4, 9, 12, 5, 6])
-	})
+describe('scale', () => {
+	expect(mat2d.scale(matA, [2, 3])).toEqual([2, 4, 9, 12, 5, 6])
+})
 
-	describe('translate', () => {
-		expect(mat2d.translate(matA, [2, 3])).toEqual([1, 2, 3, 4, 16, 22])
-	})
+describe('translate', () => {
+	expect(mat2d.translate(matA, [2, 3])).toEqual([1, 2, 3, 4, 16, 22])
+})
 
-	describe('frob', () => {
-		expect(mat2d.frob(matA)).toEqual(
-			Math.sqrt(
-				Math.pow(1, 2) +
-					Math.pow(2, 2) +
-					Math.pow(3, 2) +
-					Math.pow(4, 2) +
-					Math.pow(5, 2) +
-					Math.pow(6, 2) +
-					1
-			)
+describe('frob', () => {
+	expect(mat2d.frob(matA)).toEqual(
+		Math.sqrt(
+			Math.pow(1, 2) +
+				Math.pow(2, 2) +
+				Math.pow(3, 2) +
+				Math.pow(4, 2) +
+				Math.pow(5, 2) +
+				Math.pow(6, 2) +
+				1
 		)
+	)
+})
+
+describe('add', () => {
+	expect(mat2d.add(matA, matB)).toEqual([8, 10, 12, 14, 16, 18])
+})
+
+describe('subtract', () => {
+	expect(mat2d.subtract(matA, matB)).toEqual([-6, -6, -6, -6, -6, -6])
+})
+
+describe('multiplyScalar', () => {
+	expect(mat2d.multiplyScalar(matA, 2)).toEqual([2, 4, 6, 8, 10, 12])
+})
+
+describe('multiplyScalarAndAdd', () => {
+	expect(mat2d.multiplyScalarAndAdd(matA, matB, 0.5)).toEqual([
+		4.5, 6, 7.5, 9, 10.5, 12,
+	])
+})
+
+describe('exactEquals', () => {
+	const matA: Mat2d = [0, 1, 2, 3, 4, 5]
+	const matB: Mat2d = [0, 1, 2, 3, 4, 5]
+	const matC: Mat2d = [1, 2, 3, 4, 5, 6]
+
+	it('should return true for identical matrices', () => {
+		expect(mat2d.exactEquals(matA, matB)).toBe(true)
 	})
-
-	describe('add', () => {
-		expect(mat2d.add(matA, matB)).toEqual([8, 10, 12, 14, 16, 18])
+	it('should return false for different matrices', () => {
+		expect(mat2d.exactEquals(matA, matC)).toBe(false)
 	})
+})
 
-	describe('subtract', () => {
-		expect(mat2d.subtract(matA, matB)).toEqual([-6, -6, -6, -6, -6, -6])
+describe('equals', () => {
+	const matA: Mat2d = [0, 1, 2, 3, 4, 5]
+	const matB: Mat2d = [0, 1, 2, 3, 4, 5]
+	const matC: Mat2d = [1, 2, 3, 4, 5, 6]
+	const matD: Mat2d = [1e-16, 1, 2, 3, 4, 5]
+
+	it('should return true for identical matrices', () => {
+		expect(mat2d.equals(matA, matB)).toBe(true)
 	})
-
-	describe('multiplyScalar', () => {
-		expect(mat2d.multiplyScalar(matA, 2)).toEqual([2, 4, 6, 8, 10, 12])
+	it('should return false for different matrices', () => {
+		expect(mat2d.equals(matA, matC)).toBe(false)
 	})
-
-	describe('multiplyScalarAndAdd', () => {
-		expect(mat2d.multiplyScalarAndAdd(matA, matB, 0.5)).toEqual([
-			4.5, 6, 7.5, 9, 10.5, 12,
-		])
-	})
-
-	describe('exactEquals', () => {
-		const matA: Mat2d = [0, 1, 2, 3, 4, 5]
-		const matB: Mat2d = [0, 1, 2, 3, 4, 5]
-		const matC: Mat2d = [1, 2, 3, 4, 5, 6]
-
-		it('should return true for identical matrices', () => {
-			expect(mat2d.exactEquals(matA, matB)).toBe(true)
-		})
-		it('should return false for different matrices', () => {
-			expect(mat2d.exactEquals(matA, matC)).toBe(false)
-		})
-	})
-
-	describe('equals', () => {
-		const matA: Mat2d = [0, 1, 2, 3, 4, 5]
-		const matB: Mat2d = [0, 1, 2, 3, 4, 5]
-		const matC: Mat2d = [1, 2, 3, 4, 5, 6]
-		const matD: Mat2d = [1e-16, 1, 2, 3, 4, 5]
-
-		it('should return true for identical matrices', () => {
-			expect(mat2d.equals(matA, matB)).toBe(true)
-		})
-		it('should return false for different matrices', () => {
-			expect(mat2d.equals(matA, matC)).toBe(false)
-		})
-		it('should return true for close but not identical matrices', () => {
-			expect(mat2d.equals(matA, matD)).toBe(true)
-		})
+	it('should return true for close but not identical matrices', () => {
+		expect(mat2d.equals(matA, matD)).toBe(true)
 	})
 })
