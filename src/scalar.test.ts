c4716fdd7f3bc11fbd828088f81a61dc9e249126
Baku Hashimoto <mail@baku89.com>
2023-09-18 09:21:25 +0900

    Add scalar module

---
+++ src/scalar.test.ts
@@ -0,0 +1,114 @@
+import {EPSILON} from './common'
+import * as scalar from './scalar'
+
+test('add', () => {
+	expect(scalar.add()).toStrictEqual(0)
+	expect(scalar.add(2)).toStrictEqual(2)
+	expect(scalar.add(2, 3)).toStrictEqual(5)
+	expect(scalar.add(2, 3, 4)).toStrictEqual(9)
+})
+
+test('ceil', () => {
+	expect(scalar.ceil(5.2)).toStrictEqual(6)
+})
+
+test('distance', () => {
+	expect(scalar.distance(1, 4)).toEqual(3)
+	expect(scalar.distance(4, 1)).toEqual(3)
+	expect(scalar.distance(-1, -1)).toEqual(0)
+})
+
+test('divide', () => {
+	expect(scalar.divide()).toEqual(1)
+	expect(scalar.divide(5)).toEqual(1 / 5)
+	expect(scalar.divide(8, 2)).toEqual(4)
+	expect(scalar.divide(8, 2, 2)).toEqual(2)
+})
+
+test('equals', () => {
+	expect(scalar.equals(3 + EPSILON, 3)).toBe(true)
+	expect(scalar.equals(3 + EPSILON * 10, 3)).toBe(false)
+})
+
+test('exactEquals', () => {
+	expect(scalar.exactEquals(3, 3)).toBe(true)
+	expect(scalar.exactEquals(3 + EPSILON, 3)).toBe(false)
+})
+
+test('floor', () => {
+	expect(scalar.floor(5.2)).toEqual(5)
+})
+
+test('inverse', () => {
+	expect(scalar.inverse(2)).toEqual(0.5)
+})
+
+test('length', () => {
+	expect(scalar.length(3)).toEqual(3)
+	expect(scalar.length(0)).toEqual(0)
+	expect(scalar.length(-2)).toEqual(2)
+})
+
+test('lerp', () => {
+	expect(scalar.lerp(3, 5, 0.25)).toEqual(3.5)
+})
+
+test('max', () => {
+	expect(scalar.max(3, 7)).toEqual(7)
+})
+
+test('min', () => {
+	expect(scalar.min(3, 7)).toEqual(3)
+})
+
+test('clamp', () => {
+	expect(scalar.clamp(3, 4, 5)).toEqual(4)
+})
+
+test('multiply', () => {
+	expect(scalar.multiply()).toEqual(1)
+	expect(scalar.multiply(3)).toEqual(3)
+	expect(scalar.multiply(3, 4)).toEqual(12)
+	expect(scalar.multiply(3, 4, 5)).toEqual(60)
+})
+
+test('negate', () => {
+	expect(scalar.negate(0)).toEqual(0)
+	expect(scalar.negate(3)).toEqual(-3)
+})
+
+test('normalize', () => {
+	expect(scalar.normalize(2)).toEqual(1)
+	expect(scalar.normalize(-4)).toEqual(-1)
+	expect(scalar.normalize(0)).toEqual(0)
+})
+
+test('round', () => {
+	expect(scalar.round(5.2)).toEqual(5)
+	expect(scalar.round(-20.6)).toEqual(-21)
+	expect(scalar.round(-20.5)).toEqual(-21)
+	expect(scalar.round(-20.4)).toEqual(-20)
+})
+
+test('scale', () => {
+	expect(scalar.scale(3, 2)).toEqual(6)
+})
+
+test('scaleAndAdd', () => {
+	expect(scalar.scaleAndAdd(3, 4, 2)).toEqual(11)
+})
+
+test('squaredDistance', () => {
+	expect(scalar.squaredDistance(3, 5)).toEqual(4)
+})
+
+test('squaredLength', () => {
+	expect(scalar.squaredLength(3)).toEqual(9)
+})
+
+test('subtract', () => {
+	expect(scalar.subtract()).toEqual(0)
+	expect(scalar.subtract(5)).toEqual(-5)
+	expect(scalar.subtract(5, 2)).toEqual(3)
+	expect(scalar.subtract(5, 2, 1)).toEqual(2)
+})
---
+++ src/scalar.ts
@@ -0,0 +1,92 @@
+import * as Common from './common'
+
+export function add(...ss: number[]): number {
+	return ss.reduce((a, b) => a + b, 0)
+}
+
+export function subtract(...ss: number[]): number {
+	if (ss.length === 0) {
+		return 0
+	} else if (ss.length === 1) {
+		return -ss[0]
+	}
+	return ss.reduce((a, b) => a - b)
+}
+
+export function multiply(...ss: number[]): number {
+	return ss.reduce((a, b) => a * b, 1)
+}
+
+export function divide(...ss: number[]): number {
+	if (ss.length === 0) {
+		return 1
+	} else if (ss.length === 1) {
+		return 1 / ss[0]
+	}
+	return ss.reduce((a, b) => a / b)
+}
+
+export const ceil = Math.ceil
+
+export const floor = Math.floor
+
+export const min = Math.min
+
+export const max = Math.max
+
+export function clamp(s: number, min: number, max: number): number {
+	return Math.max(min, Math.min(max, s))
+}
+
+/**
+ * symmetric round the given number
+ */
+export const round = Common.round
+
+export function scale(a: number, s: number): number {
+	return a * s
+}
+
+export function scaleAndAdd(a: number, b: number, s: number): number {
+	return a + b * s
+}
+
+export function distance(a: number, b: number): number {
+	return Math.abs(a - b)
+}
+
+export function squaredDistance(a: number, b: number): number {
+	return (a - b) ** 2
+}
+
+export const length = Math.abs
+
+export function squaredLength(a: number): number {
+	return a ** 2
+}
+
+export function negate(a: number): number {
+	return -a
+}
+
+export function inverse(a: number): number {
+	return 1 / a
+}
+
+export function normalize(a: number): number {
+	return a === 0 ? 0 : a / Math.abs(a)
+}
+
+export function lerp(a: number, b: number, t: number): number {
+	return a + (b - a) * t
+}
+
+export function exactEquals(a: number, b: number): boolean {
+	return a === b
+}
+
+export function equals(a: number, b: number): boolean {
+	return (
+		Math.abs(a - b) <= Common.EPSILON * Math.max(1.0, Math.abs(a), Math.abs(b))
+	)
+}
