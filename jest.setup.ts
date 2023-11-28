3e61e6926c57c8e6a904e7e201554cb43269dcd4
Baku Hashimoto <mail@baku89.com>
2023-11-28 13:05:57 +0900

    Update jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,21 +2,11 @@ import {expect} from '@jest/globals'
 
 import {EPSILON} from './src/common'
 
-function areNumericVectorEqual(a: unknown, b: unknown) {
-	if (!Array.isArray(a) || !Array.isArray(b) || a.length !== b.length) {
-		return undefined
-	}
-
-	for (let i = 0; i < a.length; i++) {
-		const nearlyEqual =
-			Math.abs(a[i] - b[i]) <=
-			EPSILON * Math.max(1, Math.abs(a[i]), Math.abs(b[i]))
-
-		if (!nearlyEqual) {
-			return undefined
-		}
-	}
-	return true
+function nearlyEqual(a: number, b: number) {
+	return (
+		Math.abs(a - b) <= EPSILON * Math.max(1, Math.abs(a), Math.abs(b)) ||
+		undefined
+	)
 }
 
-expect.addEqualityTesters([areNumericVectorEqual])
+expect.addEqualityTesters([nearlyEqual])
