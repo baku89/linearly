c28acb3b2475487588f4253e35a6f137029be927
Baku Hashimoto <mail@baku89.com>
2025-03-27 21:25:24 +0900

    Update the page styles

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,12 +13,11 @@ export default defineUserConfig({
 			'link',
 			{rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true},
 		],
-		['link', {rel: 'stylesheet', href: 'https://use.typekit.net/xhr6teg.css'}],
 		[
 			'link',
 			{
 				rel: 'stylesheet',
-				href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500&family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
+				href: 'https://fonts.googleapis.com/css2?family=Geist:wght@300,400;500;600&family=Geist+Mono:wght@400;500&&family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
 				crossorigin: 'anonymous',
 			},
 		],
